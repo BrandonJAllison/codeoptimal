217e19c16ba028ca97edc061e93111226fca0af4
BrandonJAllison <brandona8128@outlook.com>
2021-10-18 20:40:29 -0400

    toat notifications

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -8,11 +8,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'antd/dist/antd.css'
 import '../public/css/styles.css'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import TopNav from '../components/TopNav.js'
 
 function MyApp ({Component,pageProps}) {
     return(
     <>
+    <ToastContainer position="top-center"/>
     <TopNav/>
     <Component {...pageProps} />;
     </>
