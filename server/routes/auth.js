0137df7183a085bf55e326867f7936b969a4d83e
Brandon Allison <brandona8128@outlook.com>
2021-10-26 15:36:37 -0400

    Email reset functionality completed
    Added functionality to password reset on server and client side

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,13 +5,14 @@ const router = express.Router();
 import { requireSignin } from "../middleware";
 
 // controllers
-import { register, login, logout, currentUser, sendTestEmail } from "../controllers/auth";
+import { register, login, logout, currentUser, forgotPassword, resetPassword } from "../controllers/auth";
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
 router.get("/current-user", requireSignin, currentUser);
-router.get("/send-email", sendTestEmail)
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 
 
 module.exports = router;
